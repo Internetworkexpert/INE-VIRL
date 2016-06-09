321c2a671c817adeb061dd756a49b47337e92ad1
Richard McLain <rmclain@ine.com>
2016-06-09 12:45:10 -0400

    Updating wrapper calls

--- RSv5.Graded.Labs/wrapper.sh
+++ RSv5.Graded.Labs/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y expect
 
-declare -a commands=("CFG R1 \"show run\"" "CFG R2 \"show version\"")
+declare -a commands=('CFG R1 "show run"' 'CFG R2 "show version"')
 
 for i in "${commands[@]}"
 do
