69f243e49e8592f59e493a023a35bf9ff9158509
bmcgahan <bmcgahan@ine.com>
2016-06-09 20:18:05 -0500

    commit

--- RSv5.Graded.Labs/wrapper.sh
+++ RSv5.Graded.Labs/wrapper.sh
@@ -2,12 +2,13 @@
 sudo apt-get install -y expect
 chmod a+x command.sh
 
-declare -a commands=('CFG R1 "show run"' 'CFG R2 "show version"')
-
-for i in "${commands[@]}"
-        do
-        eval "./command.sh $i"
-done
+# commenting out auto command runs
+# declare -a commands=('CFG R1 "show run"' 'CFG R2 "show version"')
+# 
+# for i in "${commands[@]}"
+#         do
+#         eval "./command.sh $i"
+# done
 
 PS1="command> "
 echo Usage is './command.sh CFG R1 "show ip route"'
